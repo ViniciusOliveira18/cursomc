e31b6f970b8c19479dbd1024f3c7343c5cf63a14
ViniciusOliveira18 <viniciusoliveira1808@gmail.com>
2021-11-02 02:02:58 -0300

    Produto e associacao muitos para muitos

--- src/main/java/com/vodevelopment/cursomc/domain/Categoria.java
+++ src/main/java/com/vodevelopment/cursomc/domain/Categoria.java
@@ -1,12 +1,15 @@
 package com.vodevelopment.cursomc.domain;
 
 import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Objects;
 
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.ManyToMany;
 
 @Entity
 public class Categoria implements Serializable{
@@ -17,6 +20,8 @@ public class Categoria implements Serializable{
 	private Integer id;
 	private String nome;
 	
+	@ManyToMany(mappedBy = "categorias")
+	private List<Produto> produtos = new ArrayList<Produto>();
 	
 	@Override
 	public int hashCode() {
@@ -64,6 +69,16 @@ public class Categoria implements Serializable{
 
 	public void setNome(String nome) {
 		this.nome = nome;
+	}
+
+
+	public List<Produto> getProdutos() {
+		return produtos;
+	}
+
+
+	public void setProdutos(List<Produto> produtos) {
+		this.produtos = produtos;
 	}
 	
 	
