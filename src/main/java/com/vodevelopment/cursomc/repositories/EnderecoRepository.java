f608a7ab9304bd5b97636b403106bf32440ea628
ViniciusOliveira18 <viniciusoliveira1808@gmail.com>
2021-11-03 03:16:37 -0300

    Cliente, TipoCliente, telefones e enderecos

---
+++ src/main/java/com/vodevelopment/cursomc/repositories/EnderecoRepository.java
@@ -0,0 +1,13 @@
+package com.vodevelopment.cursomc.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.vodevelopment.cursomc.domain.Endereco;
+
+@Repository
+public interface EnderecoRepository extends JpaRepository<Endereco, Integer> {
+	
+	
+
+}
